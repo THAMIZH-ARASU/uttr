6ee84c650333c4c3273de11b3a0b7293b4769915
THAMIZH-ARASU <amizharasu@gmail.com>
2025-12-20 11:52:19 +0530

    Creating constants; Added possible errors

---
+++ errors/illegal_character.py
@@ -0,0 +1,6 @@
+from errors.error import Error
+
+
+class IllegalCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
